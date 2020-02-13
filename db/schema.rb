9ed57c041239bf4b0166a0fec0ecff7fbb5ee286
Joseph Ruiz <ruizj3@uw.edu>
2020-02-13 02:31:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,5 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 1) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "age"
+    t.string "hometown"
+  end
 
 end

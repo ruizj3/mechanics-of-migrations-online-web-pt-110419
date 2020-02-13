e7372a622849fe8a7717e3bafde50f1f904a7387
Joseph Ruiz <ruizj3@uw.edu>
2020-02-13 02:35:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,4 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-    t.string "genre"
-    t.integer "age"
-    t.string "hometown"
-  end
-
 end
